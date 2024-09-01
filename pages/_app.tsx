d072222251362b3a63941359b2deca83db1ffd62
Denes Bajnok <d.bajnok@gmail.com>
2024-09-01 13:27:04 +0200

    feat: testing pwa capabilities

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import { Provider } from "react-redux";
 import store, { persistor } from "../app/store";
-import { NextComponentType, NextPageContext } from "next";
+import { Metadata, NextComponentType, NextPageContext } from "next";
 import Head from "next/head";
 import { PersistGate } from "redux-persist/integration/react";
 import "@/screens/style.css";
+
+export const metadata: Metadata = {
+  title: "Rikiki Számláló",
+  description: "Rikiki Számláló",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [{ rel: "apple-touch-icon", url: "/apple-touch-icon.png" }],
+};
 
 function MyApp({
   Component,
