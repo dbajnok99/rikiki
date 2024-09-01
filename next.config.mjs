77bf4ed48c9e8437dee723913dfa92fb60391db5
Denes Bajnok <d.bajnok@gmail.com>
2024-09-01 13:36:29 +0200

    testing if pwa wokrs correctly

--- next.config.mjs
+++ next.config.mjs
@@ -8,5 +8,6 @@ export default withPWA({
     disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
     register: true,         // register the PWA service worker
     skipWaiting: true,      // skip waiting for service worker activation
+    buildExcludes: [/app-build-manifest.json$/]
 })(nextConfig);
 
