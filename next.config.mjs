50be4a417b23458fa72e8d701f4493525eb8a3be
dbajnok99 <d.bajnok@gmail.com>
2026-03-05 12:25:25 +0100

    removing vulnerabilities

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,3 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-export default withPWA({
-    dest: "public",         // destination directory for the PWA files
-    disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
-    register: true,         // register the PWA service worker
-    skipWaiting: true,      // skip waiting for service worker activation
-    buildExcludes: [/app-build-manifest.json$/]
-})(nextConfig);
-
+export default nextConfig;
